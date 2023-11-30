c450b0804013208bf494021fb37cdca7986e977a
jdelberm <juandb95@gmail.com>
2023-11-30 08:34:43 +0100

    fix: norminette validated

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,17 +6,16 @@
 /*   By: judelgad <judelgad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 10:40:57 by judelgad          #+#    #+#             */
-/*   Updated: 2023/11/29 10:40:57 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/11/30 08:34:34 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strlen(char *str)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
-//TODO: Verify the file is norminette compliant
