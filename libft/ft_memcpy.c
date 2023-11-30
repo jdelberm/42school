bf07d8558d63991d8a57fe10be391772969d2000
jdelberm <juandb95@gmail.com>
2023-11-30 08:32:54 +0100

    fix: norminette validated

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,15 +6,15 @@
 /*   By: judelgad <judelgad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 15:21:17 by judelgad          #+#    #+#             */
-/*   Updated: 2023/11/29 17:14:48 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/11/29 19:28:13 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//TODO: comment main after testing
-//TODO: review code in pythontutor
-//TODO: review norminette
-#include <string.h>
+// TODO: comment main after testing
+// TODO: review code in pythontutor
+// TODO: review norminette
 #include <stdio.h>
+#include <string.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
@@ -22,10 +22,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char	*str_dest;
 	char	*str_src;
 
-	str_dest = (char *) dest;
-	str_src = (char *) src;
+	str_dest = (char *)dest;
+	str_src = (char *)src;
 	i = 0;
-	while(i < n)
+	while (i < n)
 	{
 		str_dest[i] = str_src[i];
 		i++;
@@ -33,12 +33,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 }
 
 int	main(void)
-{	
-	char	dest[33] = "It was a pleasure, I have to go!";
-	char 	dest2[33] = "It was a pleasure, I have to go!";
-	char 	*src = "Please real quick";
-	size_t	nbytes = 17;
-	
+{
+	char dest[33] = "It was a pleasure, I have to go!";
+	char dest2[33] = "It was a pleasure, I have to go!";
+	char *src = "Please real quick";
+	size_t nbytes = 17;
+
 	printf("Standard: before\t=> '%s'\n", dest);
 	memcpy(dest, src, nbytes);
 	printf("Standard: after\t\t=> '%s'\n\n", dest);
@@ -48,23 +48,23 @@ int	main(void)
 	printf("Custom: after\t=> '%s'\n\n", dest2);
 
 	printf("Aditional: simulating overlap\n\n");
-	char	sentence[6] = "abcde";
-	char	sentence2[6] = "abcde";
-	char	*pdest;
-	char	*psrc;
+	char sentence[6] = "abcde";
+	char sentence2[6] = "abcde";
+	char *pdest;
+	char *psrc;
 
 	nbytes = 3;
-	
+
 	pdest = &sentence[2];
 	psrc = &sentence[0];
-	
+
 	printf("Standard: before\t=> '%s'\n", sentence);
 	memcpy(pdest, psrc, nbytes);
 	printf("Standard: after\t\t=> '%s'\n\n", sentence);
 
 	pdest = &sentence2[2];
 	psrc = &sentence2[0];
-	
+
 	printf("Custom: before\t=> '%s'\n", sentence2);
 	ft_memcpy(pdest, psrc, nbytes);
 	printf("Custom: after\t=> '%s'\n\n", sentence2);
