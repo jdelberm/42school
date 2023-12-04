07e1cfbfe92b0d03159f5ab569246f8233dbaef6
jdelberm <juandb95@gmail.com>
2023-12-04 19:20:09 +0100

    fix: working version with malloc

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,11 +6,12 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 15:53:00 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/02 21:33:41 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/04 19:19:56 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <signal.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -19,6 +20,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	char	*str_dest;
 	size_t	i;
 
+	if (!dest && !src)
+		return (dest);
+	else if (!dest || !src)
+		raise(SIGSEGV);
 	tmp = malloc(n * sizeof(char));
 	str_src = (char *)src;
 	str_dest = (char *)dest;
