5542719367840c0511ba8ffdbe59cb970ffe3b59
jdelberm <juandb95@gmail.com>
2023-12-05 23:21:59 +0100

    chore: added strtrim

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:55:11 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/05 17:30:17 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/05 17:38:29 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
