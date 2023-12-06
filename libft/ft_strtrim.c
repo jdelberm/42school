c21a0d641114e219b70fa724784afa361e0af31b
jdelberm <juandb95@gmail.com>
2023-12-06 01:33:17 +0100

    chore: function under 25 lines

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,23 +6,17 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:39:02 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/05 22:55:35 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/06 01:33:01 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+int	ft_find_start(char const *s1, char const *set)
 {
-	char	*res;
 	size_t	i;
 	size_t	start;
-	size_t	end;
 
-	if (*s1 == 0)
-		return (ft_strdup(""));
-	if (*set == 0)
-		return (ft_strdup(s1));
 	start = 0;
 	i = 0;
 	while (set[i])
@@ -35,6 +29,13 @@ char	*ft_strtrim(char const *s1, char const *set)
 		}
 		i++;
 	}
+	return (start);
+}
+int	ft_find_end(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	end;
+
 	end = ft_strlen(s1) - 1;
 	i = 0;
 	while (set[i])
@@ -47,6 +48,20 @@ char	*ft_strtrim(char const *s1, char const *set)
 		}
 		i++;
 	}
+	return (end);
+}
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*res;
+	size_t	start;
+	size_t	end;
+
+	if (*s1 == 0)
+		return (ft_strdup(""));
+	if (*set == 0)
+		return (ft_strdup(s1));
+	start = ft_find_start(s1, set);
+	end = ft_find_end(s1, set);
 	res = ft_substr(s1, start, end + 1 - start);
 	if (!res)
 		return (0);
