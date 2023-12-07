6824cf9cdc00accfc40124febc6a1e4464f4c91b
jdelberm <juandb95@gmail.com>
2023-12-08 00:59:09 +0100

    chore: ft_split: little refactoring

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 10:07:06 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/07 21:17:24 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/08 00:58:31 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@
 
 // TODO: revisar las llamadas a free donde no se permita la función
 
-char *ft_trim_char(const char *str, char delim)
+char	*ft_trim_char(const char *str, char delim)
 {
-	char *str_delim;
-	char *trimmed;
+	char	*str_delim;
+	char	*trimmed;
 
 	str_delim = ft_calloc(2, sizeof(char));
-	if(!str_delim)
+	if (!str_delim)
 		return (0);
 	str_delim[0] = delim;
 	trimmed = ft_strtrim(str, str_delim);
@@ -38,12 +38,12 @@ char *ft_trim_char(const char *str, char delim)
 	return (trimmed);
 }
 
-char	*ft_get_word(char *str, char delim, size_t start, size_t len)
+char	*ft_get_word(char const *s, char delim, size_t start, size_t len)
 {
 	char	*substr;
 	char	*trimmed;
 
-	substr = ft_substr(str, start, len);
+	substr = ft_substr(s, start, len);
 	if (!substr)
 		return (0);
 	trimmed = ft_trim_char(substr, delim);
@@ -53,23 +53,11 @@ char	*ft_get_word(char *str, char delim, size_t start, size_t len)
 	return (trimmed);
 }
 
-void	ft_free_splits(char **splits, size_t len)
+size_t	ft_count_splitters(char *str, char c)
 {
-	while (len-- > 0)
-		free(splits[len]);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**splits;
-	char	*str;
-	char	*word;
-	size_t	i;
-	size_t	j;
-	size_t	k;
 	size_t	count;
+	size_t	i;
 
-	str = ft_trim_char(s, c);
 	if (!str)
 		return (0);
 	i = 0;
@@ -87,6 +75,34 @@ char	**ft_split(char const *s, char c)
 			i++;
 		}
 	}
+	return (count);
+}
+
+char	**ft_free(char *str, char **splits, size_t len)
+{
+	if (str)
+		free(str);
+	while (len-- > 0)
+		if (splits[len])
+			free(splits[len]);
+	if (splits)
+		free(splits);
+	return (0);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**splits;
+	char	*str;
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	size_t	count;
+
+	str = ft_trim_char(s, c);
+	if (!str)
+		return (0);
+	count = ft_count_splitters(str, c);
 	splits = (char **)ft_calloc(count + 1, sizeof(char *));
 	if (!splits)
 	{
@@ -98,33 +114,20 @@ char	**ft_split(char const *s, char c)
 	k = 0;
 	while (str[i])
 	{
-		if (str[i] == c)
+		if (str[i] == c && str[i+1] != c)
 		{
-			word = ft_get_word(str, c, j, i - j);
-			if (!word)
-			{
-				free(str);
-				ft_free_splits(splits, count + 1);
-				free(splits);
-				return (0);
-			}
-			splits[k++] = word;
+			splits[k] = ft_get_word(str, c, j, i - j);
+			if (!splits[k])
+				return (ft_free(str, splits, count + 1));
+			k++;
 			j = i;
-			while (str[i] == c)
-				i++;
 		}
 		i++;
 		if (!str[i])
 		{
-			word = ft_get_word(str, c, j, i - j);
-			if (!word)
-			{
-				free(str);
-				ft_free_splits(splits, count + 1);
-				free(splits);
-				return (0);
-			}
-			splits[k] = word;
+			splits[k] = ft_get_word(str, c, j, i - j);
+			if (!splits[k])
+				return (ft_free(str, splits, count + 1));
 		}
 	}
 	free(str);
