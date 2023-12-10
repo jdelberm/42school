8cfa2fabcd6f70c99d4957d6b1413b67cab3b3b3
jdelberm <juandb95@gmail.com>
2023-12-10 22:54:59 +0100

    fix: ft_lstadd_front: correction in the assignation

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,13 +6,16 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 20:30:53 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/10 20:41:28 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/10 22:54:25 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/**
+ * Update lst with the new list, placing it as the first one.
+*/
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	**lst = *new;
+	new->next = *lst;
+	*lst = new;
 }
