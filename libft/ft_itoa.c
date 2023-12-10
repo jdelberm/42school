6074a4a333243d918b32635b4ed277f8f491a426
jdelberm <juandb95@gmail.com>
2023-12-10 10:37:31 +0100

    feat: ft_itoa: pre-paco implementation

---
+++ libft/ft_itoa.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 08:12:26 by judelgad          #+#    #+#             */
+/*   Updated: 2023/12/10 10:37:11 by judelgad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_nbrlen(int n)
+{
+	size_t	nlen;
+
+	nlen = 1;
+	if (n < 0)
+	{
+		nlen++;
+		n = -n;
+	}
+	while (n > 9)
+	{
+		n /= 10;
+		nlen++;
+	}
+	return (nlen);
+}
+/*
+// Find the string size
+//	Get the natural part of the number
+//	Get the decimal part of the number
+// Allocate the string
+*/
+/*
+// While the number is greater than 9
+//	Divide it by 10
+//	If lower than 10
+//		Convert to character and add to the string
+//	Else if higher or equal than 10
+//		Divide again
+//	Get the mod of the operation
+// Return the string
+*/
+char	*ft_itoa(int n)
+{
+	char	*s;
+	size_t	nlen;
+
+	nlen = ft_nbrlen(n);
+	if (n == -2147483648)
+		return ("-2147483648");
+	s = ft_calloc(nlen + 1, sizeof(char));
+	if (!s)
+		return (0);
+	if (n < 0)
+	{
+		s[0] = '-';
+		n = -n;
+	}
+	while (n > 0)
+	{
+		s[--nlen] = (n % 10) + 48;
+		n /= 10;
+	}
+	return (s);
+}
+/*
+int	main(void)
+{
+	int	n;
+
+	n = 2147483647;
+	printf("Itoa of %i => \"%s\"\n", n, ft_itoa(n));
+}*/
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:55:11 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/06 10:07:37 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/10 08:13:18 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
