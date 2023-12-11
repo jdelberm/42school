1d77c92ce2430d1b3620adb2778d09f768ffb37f
jdelberm <juandb95@gmail.com>
2023-12-11 08:55:11 +0100

    feat: ft_lstclear: paco & norminette compliant

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 08:25:26 by judelgad          #+#    #+#             */
+/*   Updated: 2023/12/11 08:55:08 by judelgad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+	t_list	*aux;
+
+	tmp = *lst;
+	if (!tmp)
+	{
+		free(tmp);
+	}
+	else
+	{
+		while (tmp)
+		{
+			del(tmp->content);
+			aux = tmp->next;
+			free(tmp);
+			tmp = aux;
+		}
+		*lst = NULL;
+	}
+}
