8d3096cd89cdf09c0ca9afcc8735de2ff4680b19
Judelgad <judelgad@student.42malaga.com>
2023-12-13 20:20:28 +0100

    chore: norminette compliant

--- libft/libft_bonus.h
+++ libft/libft_bonus.h
@@ -6,15 +6,15 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:55:11 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/13 14:02:54 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/13 20:20:09 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_BONUS_H
 # define LIBFT_BONUS_H
 
-# include <stdlib.h>
 # include <stddef.h>
+# include <stdlib.h>
 
 typedef struct s_list
 {
@@ -27,8 +27,8 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void*));
-void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
