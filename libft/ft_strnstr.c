a4a44fe1f597ece4445254264e111f0b1b6835fb
judelgad <judelgad@student.42malaga.com>
2023-12-14 19:57:05 +0100

    track: commit of this m

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 20:24:39 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/07 06:21:55 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/14 19:50:24 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,43 +34,52 @@ char	*ft_strnstr(char *str, char *to_find, size_t len)
 		return (str);
 	found = 0;
 	i = -1;
+	// MZIRIMBZIMZIRIMBZE123
 	while (str[++i] && i < len && !found)
 	{
+		printf("char at i:%li => %c\n", i, str[i]);
 		if (str[i] == to_find[0])
 		{
+			//MZIRIMBZE
 			j = 0;
-			while (to_find[j] && str[i + j] == to_find[j])
+			while (to_find[j] && str[i + j] == to_find[j] && i + j < len)
+			{
+				printf("\tchar at i+j: %li => %c", i + j, str[i+j]);
+				printf("char at j: %li => %c\n", j, to_find[j]);
 				j++;
-			if (j == tf_size)
+			}
+			if (!to_find[j])
 				found = &str[i];
 		}
+
 	}
 	return (found);
 }
-/*
+
 int	main(void)
 {
+/*
 	char haystack[] = "Buscando la aguja en el pajar";
 	char needle[] = "aguja";
 
 	printf("HSTACK: <%s>\n", haystack);
 	printf("NEEDLE: <%s>\n", needle);
-	printf("RESULT (CST): <%s>\n", ft_strstr(haystack, needle));
-	printf("RESULT (STD): <%s>\n", strstr(haystack, needle));
+	printf("RESULT (CST): <%s>\n", ft_strnstr(haystack, needle));
+	printf("RESULT (STD): <%s>\n", strnstr(haystack, needle));
 
 	char haystack2[] = "Buscando la aguja en el pajar";
 	char needle2[] = "agrasergh";
 
 	printf("HSTACK: <%s>\n", haystack2);
 	printf("NEEDLE: <%s>\n", needle2);
-	printf("RESULT (CST): <%s>\n", ft_strstr(haystack2, needle2));
-	printf("RESULT (STD): <%s>\n", strstr(haystack2, needle2));
-
-	char haystack4[] = " ";
-	char needle4[] = " ";
-
+	printf("RESULT (CST): <%s>\n", ft_strnstr(haystack2, needle2));
+	printf("RESULT (STD): <%s>\n", strnstr(haystack2, needle2));
+*/
+	char *haystack4 = "MZIRIBMZIRIBMZE123";
+	char *needle4 = "MZIRIBMZE";
+	
 	printf("HSTACK: %s\n", haystack4);
 	printf("NEEDLE: %s\n", needle4);
-	printf("RESULT (CST): <%s>\n", ft_strstr(haystack4, needle4));
-	printf("RESULT (STD): <%s>\n", strstr(haystack4, needle4));
-}*/
+	printf("RESULT (CST): <%s>\n", ft_strnstr(haystack4, needle4, ft_strlen(needle4)));
+	printf("RESULT (STD): <%s>\n", strnstr(haystack4, needle4, ft_strlen(needle4)));
+}
