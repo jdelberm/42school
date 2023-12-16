9b49399d261b8d4f42ac775e385ef442907888d6
jdelberm <juandb95@gmail.com>
2023-12-16 08:50:24 +0100

    refactor: removed unused functions

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,68 +6,12 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 10:07:06 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/15 18:59:37 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/16 08:49:00 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
-
-/**
- * @brief Trims a character from the beginning and end of a string.
- *
- * This function takes a string and a delimiter character as input and removes
- * all occurrences of the delimiter character from the beginning and end of the
- * string.
- *
- * @param str The string to be trimmed.
- * @param delim The delimiter character to be removed.
- * @return A pointer to the trimmed string.
- */
-static char	*ft_trim_char(const char *str, char delim)
-{
-	char	*str_delim;
-	char	*trimmed;
-
-	str_delim = ft_calloc(2, sizeof(char));
-	if (!str_delim)
-		return (0);
-	str_delim[0] = delim;
-	trimmed = ft_strtrim(str, str_delim);
-	free(str_delim);
-	return (trimmed);
-}
-
-/**
- * @brief Retrieves a word from a string based on the given delimiter, start
- * index, and length.
- *
- * This function takes a string, a delimiter character, a start index, and a
- * length as input.
- * It retrieves a word from the string starting at the specified index and with
- * the specified length.
- * The word is separated by the given delimiter character.
- *
- * @param s The string from which to retrieve the word.
- * @param delim The delimiter character used to separate words.
- * @param start The starting index of the word in the string.
- * @param len The length of the word to retrieve.
- * @return A pointer to the retrieved word, or NULL if an error occurred.
- */
-static char	*ft_get_word(char const *s, char delim, size_t start, size_t len)
-{
-	char	*substr;
-	char	*trimmed;
-
-	substr = ft_substr(s, start, len);
-	if (!substr)
-		return (0);
-	trimmed = ft_trim_char(substr, delim);
-	free(substr);
-	if (!trimmed)
-		return (0);
-	return (trimmed);
-}
 
 /**
  * Counts the number of splitters in a string.
@@ -93,29 +37,6 @@ static size_t	ft_count_splitters(const char *s, char c)
 			s++;
 	}
 	return (count);
-}
-
-/**
- * @brief Frees the memory allocated for a string and an array of strings.
- * 
- * This function takes a string, an array of strings, and the number of
- * elements in the array.
- * It frees the memory allocated for the string and each element in the array.
- * 
- * @param str The string to be freed.
- * @param splits The array of strings to be freed.
- * @param len The number of elements in the array.
- */
-static char	**ft_free(char *str, char **splits, size_t len)
-{
-	if (str)
-		free(str);
-	while (len-- > 0)
-		if (splits[len])
-			free(splits[len]);
-	if (splits)
-		free(splits);
-	return (0);
 }
 
 /**
