0abbfce11c09fecc73facd29005bd129964b6b98
Judelgad <judelgad@student.42malaga.com>
2023-12-23 16:11:54 +0100

    feat: basic structure of ft_printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,11 +6,20 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:45:41 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/23 16:03:28 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/23 16:11:35 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(char const * str)
+#include <stdarg.h>
+
+int	ft_printf(char const *str, ...)
 {
-	
+	va_list	ptr;
+
+	va_start(ptr, str);
+	while (ptr)
+	{
+		va_arg(ptr, char *);
+	}
+	va_end(ptr);
 }
