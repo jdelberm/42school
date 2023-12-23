ca9ede105833a8723a66a8d42f50c44b04610ef2
Judelgad <judelgad@student.42malaga.com>
2023-12-23 18:23:47 +0100

    feat: ft_printf function structure definition

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,13 +6,35 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:45:41 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/23 16:50:03 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/23 18:18:57 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libftprintf.h"
 #include <stdarg.h>
 
 int	ft_printf(char const *str, ...)
 {
+	va_list			ptr;
+	unsigned int	count;
 
+	va_start(ptr, str);
+	count = 0;
+	while (*str)
+	{
+		if (*str == '%')
+			ft_exec_ftype(*++str, ptr, count);
+		else
+			write(1, &(*str), 1);
+		if (count == -1)
+			return (count);
+		str++;
+	}
+	va_end(ptr);
+	return (count);
 }
+/*
+int	main(void)
+{
+	ft_printf("%c", 'a');
+}*/
