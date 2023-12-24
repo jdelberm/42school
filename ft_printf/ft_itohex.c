0ed5459daad51ee526594e075fd0934c8fa5bd12
judelgad <judelgad@student.42malaga.com>
2023-12-24 02:08:05 +0100

    fix: removed mayus parameter since str can be printed outside

--- ft_printf/ft_itohex.c
+++ ft_printf/ft_itohex.c
@@ -6,18 +6,13 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 17:45:19 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/23 18:33:11 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/24 02:07:26 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_itohex(int n, int mayus)
+int	ft_itohex(int n)
 {
-	n = 1337;
-	if (mayus)
-		write(1, "M", 1);
-	else
-		write(1, "m", 1);
 	return (n);
 }
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:59:05 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/24 02:03:24 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/24 02:07:35 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		ft_putstr(char *str);
 char	*ft_itoa(int n);
 void	ft_print_pointer(void *ptr);
 int		ft_print_unsigned(unsigned int n);
-int		ft_itohex(int n, int mayus);
+int		ft_itohex(int n);
 int		ft_exec_ftype(char type, va_list *ptr, unsigned int *count);
 int		ft_printf(char const *str, ...);
 
