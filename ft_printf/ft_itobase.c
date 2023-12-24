9f0cb8e52932687acf33fdf1abafdcda908a85fd
judelgad <judelgad@student.42malaga.com>
2023-12-24 14:13:56 +0100

    fix: missed to set base quotient to base_len

--- ft_printf/ft_itobase.c
+++ ft_printf/ft_itobase.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 17:45:19 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/24 02:33:08 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/24 13:59:22 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_itobase(int n, char *base)
 	char	c;
 
 	base_len = ft_strlen(base);
-	if (n < 16)
+	if ((size_t)n < base_len)
 	{
 		c = base[n];
 		write(1, &c, 1);
