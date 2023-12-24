f087c478572a9fed96700822c0b48d292b59f8ce
judelgad <judelgad@student.42malaga.com>
2023-12-24 14:20:35 +0100

    feat: added standard print of pointer

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,12 +6,13 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:45:41 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/24 14:05:23 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/24 14:16:44 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 int	ft_printf(char const *str, ...)
 {
@@ -38,13 +39,18 @@ int	ft_printf(char const *str, ...)
 int	main(void)
 {
 	int	n;
+	int *ptr;
 
+	n=4321;
+	ptr= &n;
 	ft_printf("%c%%c%sc%i%d%c\n", 'x', "string", 1234, 321, 'y');
 	ft_printf("Printing characters =>\t\t%cXXX%ca%c\n", 'a', 'b', '3');
 	ft_printf("Printing numbers =>\t\t%ia%ib%dc\n", 123, 456, 789);
 	ft_printf("Printing strings => non_var_%s_nonvar_%s\n", "var 1", "var2@ñ");
 	ft_printf("this is an unsigned int =>\t%u\n", 4294967295);
-	n = 4321;
 	ft_printf("Number %i to hex =>\t\t%x\n", n, n);
 	ft_printf("Number %i to hex in mayus =>\t%X\n", n, n);
+
+	printf("\nSTANDARD PRINTS\n");
+	printf("Printing the address of a pointer => %p", ptr);
 }
