25133a00818e9d572db37156f7febab5aa5d2af7
judelgad <judelgad@student.42malaga.com>
2023-12-24 14:15:05 +0100

    feat: reorganized print tests

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:45:41 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/24 02:34:09 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/24 14:05:23 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,13 @@ int	ft_printf(char const *str, ...)
 
 int	main(void)
 {
-	int n;
-	ft_printf("%c%%c%sc%i%d%c\n", 'x',"string", 1234,321, 'y');
-	ft_printf("this is an unsigned int => %u\n", 4294967295);
+	int	n;
+
+	ft_printf("%c%%c%sc%i%d%c\n", 'x', "string", 1234, 321, 'y');
+	ft_printf("Printing characters =>\t\t%cXXX%ca%c\n", 'a', 'b', '3');
+	ft_printf("Printing numbers =>\t\t%ia%ib%dc\n", 123, 456, 789);
+	ft_printf("Printing strings => non_var_%s_nonvar_%s\n", "var 1", "var2@ñ");
+	ft_printf("this is an unsigned int =>\t%u\n", 4294967295);
 	n = 4321;
 	ft_printf("Number %i to hex =>\t\t%x\n", n, n);
 	ft_printf("Number %i to hex in mayus =>\t%X\n", n, n);
