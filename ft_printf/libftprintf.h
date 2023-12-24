df474049941782607117733247a21f11ea28cbfc
judelgad <judelgad@student.42malaga.com>
2023-12-24 14:25:51 +0100

    fix: typo in ft_putptr parameter

--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:59:05 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/24 14:17:22 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/24 14:21:40 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		ft_puthex(unsigned int n, char *base, int mayus);
 void	ft_putnbr(int nb);
 void	ft_putunbr(unsigned int nb);
 int		ft_putstr(char *str);
-int		ft_putptr(void * ptr);
+int		ft_putptr(void *ptr);
 char	*ft_itoa(int n);
 void	ft_print_pointer(void *ptr);
 int		ft_print_unsigned(unsigned int n);
