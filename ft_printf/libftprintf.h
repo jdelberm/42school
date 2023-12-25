d738f3f53a557d074b19ba448d3fdb23f9e0c180
jdelberm <juandb95@gmail.com>
2023-12-26 00:18:37 +0100

    refactor: removed libft functions from "libftprintf" library

--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:59:05 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/24 14:21:40 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/25 21:58:53 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,14 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-size_t	ft_strlen(char *str);
 int		ft_putchar(char c);
 int		ft_puthex(unsigned int n, char *base, int mayus);
-void	ft_putnbr(int nb);
 void	ft_putunbr(unsigned int nb);
 int		ft_putstr(char *str);
 int		ft_putptr(void *ptr);
-char	*ft_itoa(int n);
 void	ft_print_pointer(void *ptr);
 int		ft_print_unsigned(unsigned int n);
-int		ft_itobase(int n, char *base);
+int		ft_putnbr_base(int n, const char *base);
 int		ft_exec_ftype(char type, va_list *ptr, unsigned int *count);
 int		ft_printf(char const *str, ...);
 
