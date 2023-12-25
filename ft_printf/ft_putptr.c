f48f2b23f12c73e73e333b583b24a9bfcb719d6a
jdelberm <juandb95@gmail.com>
2023-12-26 00:52:11 +0100

    feat: ft_putptr: printing pointer address

--- ft_printf/ft_putptr.c
+++ ft_printf/ft_putptr.c
@@ -6,13 +6,14 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 17:44:31 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/24 02:39:08 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/26 00:51:19 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libftprintf.h"
 #include <unistd.h>
 
-void	ft_putptr(void *ptr)
+int	ft_putptr(void *ptr)
 {
-	write(1, ptr, 1);
+	return ft_puthex((long int) ptr, "0123456789abcdef", 0);
 }
