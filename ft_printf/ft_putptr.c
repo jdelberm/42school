0bff7a7487a385975aa15296f3f87f07e7ff5f0f
jdelberm <juandb95@gmail.com>
2023-12-26 11:50:51 +0100

    fix: wasn't printing hex correctly

--- ft_printf/ft_putptr.c
+++ ft_printf/ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 17:44:31 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/26 10:43:59 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/26 11:07:43 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 int	ft_putptr(void *ptr, int *count)
 {
-	ft_putstr("0x7ffe", count);
+	ft_putstr("0x", count);
 	return (ft_putnbr_base((long int)ptr, "0123456789abcdef", count));
 }
