23e5a5f157a26a29d86590c30bfca99420aeaa4d
judelgad <judelgad@student.42malaga.com>
2023-12-26 22:07:41 +0100

    fix: force count to -1 for error

--- ft_printf/ft_putstr.c
+++ ft_printf/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:49:47 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/26 22:03:59 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/26 22:07:19 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@
 void	ft_putstr(char *str, int *count)
 {
 	if(!str)
+	{
 		ft_putstr("(null)", count);
+		(*count) = -1;
+	}
 	else
 		while (*str)
 			ft_putchar(*str++, count);
