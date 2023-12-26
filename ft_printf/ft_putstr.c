d13248d9be889345543eb8aa74152c4e7e9bac53
judelgad <judelgad@student.42malaga.com>
2023-12-27 00:48:29 +0100

    fix: norminette: missing space after keyword

--- ft_printf/ft_putstr.c
+++ ft_printf/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 15:49:47 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/26 22:08:24 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/27 00:45:15 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	ft_putstr(char *str, int *count)
 {
-	if(!str)
+	if (!str)
 		ft_putstr("(null)", count);
 	else
 		while (*str)
