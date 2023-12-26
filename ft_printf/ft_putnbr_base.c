5ed7bc3da3a4caa561f47e295976b859fb7b38a7
jdelberm <juandb95@gmail.com>
2023-12-26 10:45:01 +0100

    fix: norminette: replaced space by tab

--- ft_printf/ft_putnbr_base.c
+++ ft_printf/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 17:45:19 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/26 10:43:49 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/26 10:44:51 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int	ft_putnbr_base(unsigned int n, const char *base, int *count)
 {
-	size_t base_len;
+	size_t	base_len;
 
 	base_len = ft_strlen(base);
 	if ((size_t)n < base_len)
