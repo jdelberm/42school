9de5202a8ac111864f22b99ceef3d9471aa0b3cf
judelgad <judelgad@student.42malaga.com>
2023-12-27 00:51:11 +0100

    fix: write returns -1 if fails, not 0

--- ft_printf/ft_putchar.c
+++ ft_printf/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 18:10:09 by judelgad          #+#    #+#             */
-/*   Updated: 2023/12/27 00:49:41 by judelgad         ###   ########.fr       */
+/*   Updated: 2023/12/27 00:50:57 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_putchar(char c, int *count)
 {
-	if(write(1, &c, 1))
-		*count += 1;
-	else
+	if (write(1, &c, 1) == -1)
 		*count = -1;
+	else
+		*count += 1;
 }
