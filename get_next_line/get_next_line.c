0fa4046fd58e488e0af8c5f5b1de10b433dd6c43
jdelberm <juandb95@hotmail.com>
2023-12-28 11:30:00 +0100

    chore: added a little TODO list

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,6 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//TODO: Analyze where static variables could be useful
+//TODO: Program must be compiled with flag -D BUFFER_SIZE=xx
+//TODO: Make read buffer be determined by BUFFER_SIZE value
+//TODO: Read line
+//TODO: Return NULL if read fails
+//TODO: Return NULL if EOF is reached
+//TODO: Read from file
+//TODO: Read from stdin
+//TODO: Ensure line ending with \n
+//TODO: Avoid \n ending if EOF doesn't
+//TODO: Declare util functions in GET_NEXT_LINE_UTILS
+//TODO: Ensure lseek and global variables aren't being used
+
 char	*get_next_line(int fd)
 {
 	return ("");
