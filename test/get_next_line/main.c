bea5e0bcab90cfc50f9f62a9c819e4edd0f26de1
jdelberm <jdelgado.berm@gmail.com>
2024-01-01 09:03:04 +0100

    feat: test: different way for showing line content

--- test/get_next_line/main.c
+++ test/get_next_line/main.c
@@ -1,6 +1,6 @@
 #include "../../get_next_line/get_next_line.h"
-
 #include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -8,17 +8,22 @@ int	main(void)
 {
 	char *line;
 	int fd;
+	int found;
 	int i;
 
+	printf("BUFFER SIZE => %i\n", BUFFER_SIZE);
 	fd = open("test_file.txt", O_RDONLY);
-	line = get_next_line(fd);
-	if (!line)
+	if (!fd)
 		return (0);
+	found = 3;
 	i = 0;
-	while (line[i])
+	while (found)
 	{
-		write(1, &line[i], 1);
-		i++;
+		found--;
+		line = get_next_line(fd);
+		if (!*line)
+			found = 0;
+		else
+			printf("line %i => %s\n", i++, line);
 	}
-	// free(line);
 }
