adb6b881fa3bedb9857e2a469bc9fd8057fbbc43
jdelberm <jdelgado.berm@gmail.com>
2024-01-13 20:22:05 +0100

    refactor: removed unnecessary comment

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 20:59:13 by judelgad          #+#    #+#             */
-/*   Updated: 2024/01/11 23:07:45 by judelgad         ###   ########.fr       */
+/*   Updated: 2024/01/13 20:21:38 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_read_text(int fd, int *rbytes);
-//int		ft_extract_line(char **text, char **line, int len);
 char	*ft_extract_line(char **head, char *text, int len);
 char	*get_next_line(int fd);
 
