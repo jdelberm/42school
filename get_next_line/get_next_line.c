43674be2c5cb061ed120b284da17d2d4e8075242
Juan de Dios Delgado Bermúdez <judelgad@student.42malaga.com>
2024-02-01 07:57:57 +0000

    feat: completed

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: judelgad <judelgad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 20:58:41 by judelgad          #+#    #+#             */
-/*   Updated: 2024/02/01 07:40:53 by judelgad         ###   ########.fr       */
+/*   Updated: 2024/02/01 07:56:36 by judelgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,14 +87,13 @@ int	ft_read_text(int fd, int *res, char **text)
 	char	*buffer;
 
 	*res = 1;
-	*text = 0;
 	while (*res != -1 && *res != 0)
 	{
 		buffer = malloc(BUFFER_SIZE + 1);
 		if (!buffer)
 		{
 			free(*text);
-			text = 0;
+			*text = 0;
 			return (0);
 		}
 		*res = read(fd, buffer, BUFFER_SIZE);
@@ -117,26 +116,23 @@ char	*get_next_line(int fd)
 	char		*text;
 	int			res;
 
+	text = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	if (!ft_contains_nl(str[fd]))
 	{
-		if (!ft_read_text(fd, &res, &text))
-		{
-			free(str[fd]);
-			str[fd] = 0;
+		if (!ft_read_text(fd, &res, &str[fd]))
 			return (0);
-		}
-		if ((!text && !str[fd]) || res == -1)
+		if (res == -1)
 		{
 			free(str[fd]);
 			str[fd] = 0;
 		}
-		str[fd] = ft_strjoin_and_free(str[fd], text);
-		if (!str[fd])
-			return (0);
 	}
-	text = ft_get_line(str[fd]);
-	str[fd] = ft_get_remainder(str[fd], text);
+	if (str[fd])
+	{
+		text = ft_get_line(str[fd]);
+		str[fd] = ft_get_remainder(str[fd], text);
+	}
 	return (text);
 }
